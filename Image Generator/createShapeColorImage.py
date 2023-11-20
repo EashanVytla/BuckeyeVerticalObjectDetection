754ea9c1f3f12ee3d3f0de32c8121482b589f48b
Victor <fawole.5@osu.edu>
2023-11-20 13:24:13 -0500

    Committed correct save

--- Image Generator/createShapeColorImage.py
+++ Image Generator/createShapeColorImage.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+from math import sin,cos,radians
 from time import sleep
 import os
 from PIL import Image
@@ -16,7 +17,11 @@ colors = [
     (255, 0, 0),  # Red
     (0, 0, 255),  # Blue
     (0, 255, 0),  # Green
-    (255, 0, 255),  # Purple
+    #(255, 0, 255),  # Pink
+    (254,254,254), # White
+    (128,0,128), # Purple
+    (150,75,0), # Brown
+    (255,165,0), # Orange
 ]
 
 shapeToggle = 0
@@ -28,7 +33,7 @@ wn = pygame.display.set_mode((wn_width, wn_height))
 
 pygame.display.set_caption("Image Shape Drawer(Buckeye Vertical)")
 
-max_type = 3
+max_type = 7
 
 def mainLoop():
     state = True
@@ -62,7 +67,7 @@ def mainLoop():
 
         wn.blit(text, textRect)
 
-        fileLocation = "EashanRepository\BuckeyeVerticalObjectDetection\Image Generator\images/" + str(i) + str(j) + str(c) + ".png"
+        fileLocation = "BuckeyeVerticalObjectDetection\BuckeyeVerticalObjectDetection\Image Generator\images" + str(i) + str(j) + str(c) + ".png"
         pygame.image.save(wn, fileLocation)
         # Converts white pixels to transparent
         convertImage(fileLocation)
@@ -79,11 +84,6 @@ def mainLoop():
             c = 65
         else:
             c+=1
-        
-
-        print("i: " + str(i))
-        print("j: " + str(j))
-        print("c: " + str(c))
 
         pygame.display.update()
         clock.tick(300)
@@ -111,31 +111,89 @@ def convertImage(path):
     print("Successful")
 
 def drawShape(type, color):
-    if type == 0:
+    radius = wn_width/2
+    center = [wn_width/2, wn_height/2]
+    if type == 6:
         #Rectangle
         rect_width = 500
         rect_height = 300
         pygame.draw.rect(wn, color, (wn_width/2-rect_width/2, wn_height/2-rect_height/2, rect_width, rect_height))
-    elif type == 1:
+    elif type == 5:
         #Circle
         pygame.draw.circle(wn, color, (wn_width/2, wn_height/2), wn_height/2)
-    elif type == 2:
+    elif type == 4:
         # Triangle
         # Put 5 at the end if you want it to be an empty triangle as another parameter
         pygame.draw.polygon(wn, color, [[wn_width/2 - wn_height/math.tan(math.pi/3), wn_height], [wn_width/2,0], [wn_width/2 + wn_height/math.tan(math.pi/3), wn_height]])
-    elif type == 3:
-        #Square
-        square_length = 500
-        pygame.draw.rect(wn, color, (wn_width/2-square_length/2, wn_height/2-square_length/2, square_length, square_length))
-
-    '''elif type == 2:
+    
+    elif type == 1:
         #Semicircle
+        pie(wn,color,[center[0],center[1]-150],radius,0,180)
+        
+    elif type == 0:
+        #Quarter circle
+        pie(wn,color,[70,0],wn_height,0,90)
+    elif type == 2:
+        #Pentagon
+        sides = 5
+        fit_radius = radius / (1.283)
+        pentagon_points = generate_ngon_points(sides,fit_radius,[center[0],center[1]+10])
+        pygame.draw.polygon(wn,color,pentagon_points)
     elif type == 3:
-        #Quarter circle'''
+        #Star
+        star_points = generate_star_points(radius, center)
+        pygame.draw.polygon(wn,color,star_points)
+    '''elif type == 8:
+        #Cross
+        '''
 
-    '''elif type == 5:
-        #Square
-    elif type == 6:
-        #Trapezoid'''
+def generate_star_points(outer_radius, center):
+    # Golden ratio
+    phi = (1 + math.sqrt(5)) / 2
+    
+    # Calculate the inner radius
+    inner_radius = outer_radius / phi
+
+    # The points of the star
+    points = []
+
+    # The angle between the star's points
+    angle_between_points = math.pi / 5
+
+    # Generate the points for the star
+    for i in range(5):
+        # Outer point (tip of the star)
+        outer_angle = 2 * math.pi * i / 5 - math.pi / 2
+        outer_x = center[0] + outer_radius * math.cos(outer_angle)
+        outer_y = center[1] + outer_radius * math.sin(outer_angle)
+        points.append((outer_x, outer_y))
+        
+        # Inner point (inner corner of the star)
+        inner_angle = outer_angle + angle_between_points
+        inner_x = center[0] + inner_radius * math.cos(inner_angle)
+        inner_y = center[1] + inner_radius * math.sin(inner_angle)
+        points.append((inner_x, inner_y))
+
+    return points
+
+def generate_ngon_points(sides, radius, center):
+    points = []
+    offset_angle = (3 * math.pi / 2) + (math.pi / sides)  # Start at the top of the screen
+    
+    for k in range(sides):
+        # Calculate each point of the n-gon starting from the top
+        angle = 2 * math.pi * k / sides - offset_angle
+        x = center[0] + radius * math.cos(angle)
+        y = center[1] + radius * math.sin(angle)  # Pygame's y-axis increases downwards
+        points.append((x, y))
+
+    return points
+    
+def pie(scr,color,center,radius,start_angle,stop_angle):
+    theta=start_angle
+    while theta <= stop_angle:
+        pygame.draw.line(scr,color,center, 
+        (center[0]+radius*cos(radians(theta)),center[1]+radius*sin(radians(theta))),2)
+        theta+=0.01
 
 mainLoop()
